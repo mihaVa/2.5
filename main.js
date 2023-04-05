236c68979b59f7b425379c450b0127191ee811a9
Михаил <mixa.varuxa@gmail.com>
2023-04-05 16:17:40 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+const readline = require('readline');
+const colors = require('colors')
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
